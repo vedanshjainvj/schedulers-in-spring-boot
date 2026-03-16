ef693c37c9721d29bdc05d53a1797b391f502950
vedanshjainvj <vedansh.code@gmail.com>
2026-03-16 21:47:07 +0530

    feat: configure custom ThreadPoolTaskScheduler to allow concurrent scheduled jobs

---
+++ src/main/java/com/learnscheduling/learnscheduling/config/SchedulerConfig.java
@@ -0,0 +1,19 @@
+package com.learnscheduling.learnscheduling.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulerConfig {
+
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5);
+        scheduler.setThreadNamePrefix("scheduler-thread-");
+
+        return scheduler;
+    }
+}
